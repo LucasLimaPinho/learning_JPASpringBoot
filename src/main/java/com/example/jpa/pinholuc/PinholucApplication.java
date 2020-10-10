12cb6929a35c19c50d2c7f699201549b7623d54a
Lucas Pinho <lucas.pinho@semantix.com.br>
2020-10-10 12:25:58 -0300

    Adicionar comentários

--- src/main/java/com/example/jpa/pinholuc/PinholucApplication.java
+++ src/main/java/com/example/jpa/pinholuc/PinholucApplication.java
@@ -3,6 +3,27 @@ package com.example.jpa.pinholuc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/*
+ * 
+ * 
+ * Hibernate is of the most popular ORM (Object Relation Mapping) frameworks.
+ * Hibernate is one of the popular implementations of JPA (Java Persistent API)
+ * Hibernate understands the mappings that we add between objects (beans) and tables.
+ * It ensures that data is stored/retrieved from the database based on the mappings
+ * 
+ * 
+
+	How do you define your entities?
+	
+	How do you map attributes?
+	
+	How do you map relationships between entities?
+	
+	Who manages the entities?
+
+*/
+
 @SpringBootApplication
 public class PinholucApplication {
 
