52c5fa675e463d8321a4279a58781fdf16765ce1
Lucas Pinho <lucas.pinho@semantix.com.br>
2020-10-10 12:09:37 -0300

    JPA Spring Boot - aplicações Java com Database facilitada pelo uso de annotations do framework Spring Boot que fazem o mapping entre BEANS e COLUNAS das tabelas

---
+++ src/main/java/com/example/jpa/pinholuc/PinholucApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpa.pinholuc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinholucApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinholucApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jpa/pinholuc/PinholucApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpa.pinholuc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinholucApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
